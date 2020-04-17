76e563145b6079c7cebeef824eec80d8b9f8acc0
Christopher Miles <twitch@nervestaple.com>
2020-04-17 14:57:14 -0400

    Altered for personal use and testing

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject lein-jlink "0.2.2-SNAPSHOT"
+(defproject com.nervestaple/lein-jlink "0.2.2-SNAPSHOT"
   :description "Package your Leiningen project as a standalone and stripped-down JVM using Java's jlink. Great for distribution and Docker images."
-  :url "https://github.com/sunng87/lein-jlink"
+  :url "https://github.com/cmiles74/lein-jlink"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true
-  :deploy-repositories {"releases" :clojars})
+  :eval-in-leiningen true)
